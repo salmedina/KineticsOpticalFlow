4ae782407cb019324ef1db1b78efac5fd6934af1
Salvador Medina <salvadom@andrew.cmu.edu>
2019-02-21 05:06:57 +0000

    Change step to 1 in brox script

--- src/extract_brox.sh
+++ src/extract_brox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 build_of.py /mnt/kinetics/video /mnt/kinetics/of_brox  --num_worker=6 --flow_type=brox --df_path=/home/salvadom/Devel/dense_flow --out_format=dir --ext=mp4 --frame_step=5 --num_gpu=6
+python3 build_of.py /mnt/kinetics/video /mnt/kinetics/of_brox  --num_worker=6 --flow_type=brox --df_path=/home/salvadom/Devel/dense_flow --out_format=dir --ext=mp4 --frame_step=1 --num_gpu=6
