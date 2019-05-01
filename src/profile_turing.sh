1c5ab9e1d3535413a5fd744c868e63059f0bf490
salmedina <salbed.medina@gmail.com>
2019-04-30 22:48:31 -0400

    Fix video name extraction from zsh to bash

--- src/profile_turing.sh
+++ src/profile_turing.sh
@@ -7,7 +7,8 @@ output_dir="/home/salvadom/Data/OF_Profiling/turing"
 rm -rf ${output_dir}/*
 
 for video_path in ${video_dir}/*.mp4; do
-    video_name=${video_path:t:r}
+    file_name=$(basename -- "$file")
+    video_name="${file_name%.*}"
     video_frames_dir=${frames_dir}/${video_name}
     video_output_dir=${output_dir}/${video_name}
     echo "AppOFCuda --input=${video_frames_dir}/*.jpg --output=${video_output_dir}/${video_name} --gpuIndex=0 --preset=medium"
