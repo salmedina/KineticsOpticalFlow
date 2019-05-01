0c739c69cbe2b0e4a7d03fb849257631174172a2
salmedina <salbed.medina@gmail.com>
2019-04-30 23:06:49 -0400

    Final fix profile turing

--- src/profile_turing.sh
+++ src/profile_turing.sh
@@ -11,6 +11,7 @@ for video_path in ${video_dir}/*.mp4; do
     video_name="${file_name%.*}"
     video_frames_dir=${frames_dir}/${video_name}
     video_output_dir=${output_dir}/${video_name}
-    echo "AppOFCuda --input=${video_frames_dir}/*.jpg --output=${video_output_dir}/*.flo --gpuIndex=0 --preset=medium"
-    #python3 AppOFCuda --input=${video_frames_dir}/*.jpg --output=${video_output_dir}/${video_name} --gpuIndex=0 --preset=medium
+    echo "${video_output_dir}"
+    mkdir -p ${video_output_dir}
+    python3 AppOFCuda --input=${video_frames_dir}/*.png --output=${video_output_dir} --gpuIndex=0 --preset=medium
 done
